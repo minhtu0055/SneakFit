212ee9feb3c9d3c219d1c6fa21a84501969daaf3
Admin <Admin@LAPTOP-R3R9CLAI>
2025-03-24 22:00:55 +0700

    createbackend

---
+++ SneakFit.ViewModels/Catalog/ThuongHieu/SuaThuongHieu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SneakFit.ViewModels.Catalog.ThuongHieu
+{
+    public class SuaThuongHieu
+    {
+        public Guid Id { get; set; }
+        public string TenThuongHieu { get; set; }
+    }
+}
---
+++ SneakFit.ViewModels/Catalog/ThuongHieu/ThuongHieuViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SneakFit.ViewModels.Catalog.ThuongHieu
+{
+    public class ThuongHieuViewModels
+    {
+        public Guid Id { get; set; }
+        public string TenThuongHieu { get; set; }
+    }
+}
