de6d78efc702adea7bb307900b5b0ae3494cd0e1
hoangvanthien2005 <thienhvph49820@gmail.com>
2025-05-18 10:52:38 +0700

    Thien_Configuration

---
+++ SneakFit.Data/Configuration/SanPhamConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using SneakFit.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SneakFit.Data.Configuration
+{
+    public class SanPhamConfiguration : IEntityTypeConfiguration<SanPham>
+    {
+        public void Configure(EntityTypeBuilder<SanPham> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.TenSanPham).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Mota).HasMaxLength(200);
+            builder.Property(x => x.TrangThai).IsRequired();
+        }
+    }
+}
---
+++ SneakFit.Data/Configuration/VoucherConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using SneakFit.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SneakFit.Data.Configuration
+{
+    public class VoucherConfiguration : IEntityTypeConfiguration<Voucher>
+    {
+        public void Configure(EntityTypeBuilder<Voucher> builder)
+        {
+            builder.ToTable("Vouchers");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.MaVoucher).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.GiaTriGiamGia).HasColumnType("decimal(18,2)").IsRequired();
+            builder.Property(x => x.DieuKienApDung).HasColumnType("decimal(18,2)").IsRequired();
+            builder.Property(x => x.SoLuong).IsRequired();
+            builder.Property(x => x.NgayTao).IsRequired();
+            builder.Property(x => x.ThoiGianBatDau).IsRequired();
+            builder.Property(x => x.ThoiGianKetThuc).IsRequired();
+            builder.Property(x => x.TrangThai).IsRequired();
+        }
+    }
+}
